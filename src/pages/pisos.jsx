f250e1ace4d4f8a80b079d7e0cde40bdd5597bb2
Alan Andre <114703935+AlanAndreUP@users.noreply.github.com>
2023-02-25 13:20:19 -0600

    Update pisos.jsx

--- src/pages/pisos.jsx
+++ src/pages/pisos.jsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Image from 'next/image'
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+
 export default function Home() {
   const router = useRouter();
 
@@ -16,23 +16,24 @@ export default function Home() {
     router.push('/administrador');
     alert('Inicio Correcto ');
   };
+
   return (
     <motion.div 
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 1 }}
-  >
-    <div className={styles.container}>
-         <Image
-        src="https://alanandreup.github.io/Tarea/22.png"
-        alt="background image"
-         layout="fill"
-      sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
-        quality={100}
-      />
-    
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <div className={styles.container}>
+        <Image
+          src="https://alanandreup.github.io/Tarea/22.png"
+          alt="background image"
+          layout="fill"
+          sizes="(max-width: 768px) 100vw,
+                (max-width: 1200px) 50vw,
+                33vw"
+          quality={100}
+        />
+      </div>
     </motion.div>
   );
 }
